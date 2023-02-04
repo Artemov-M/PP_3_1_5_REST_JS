e0768dccb0e0798bc91ca3d0893637ca7ba89513
Mikhail Artemov <artemov.m.p@gmail.com>
2023-02-04 07:25:59 +0400

    Solution 3.1.5

--- src/main/java/ru/kata/spring/boot_security/demo/initialization/InitDB.java
+++ src/main/java/ru/kata/spring/boot_security/demo/initialization/InitDB.java
@@ -1,43 +1,40 @@
 package ru.kata.spring.boot_security.demo.initialization;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.dto.UserCreateUpdateDto;
 import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.annotation.PostConstruct;
+import java.util.Set;
 
 @Component
 public class InitDB {
 
     private final RoleService roleService;
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
-    public InitDB(RoleService roleService, UserService userService, PasswordEncoder passwordEncoder) {
+    public InitDB(RoleService roleService, UserService userService) {
         this.roleService = roleService;
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
     private void fillDb() {
-        Role roleAdmin = new Role("ROLE_ADMIN");
-        Role roleUser = new Role("ROLE_USER");
+        Role roleAdmin = roleService.add(new Role("ROLE_ADMIN"));
+        Role roleUser = roleService.add(new Role("ROLE_USER"));
 
-        User admin = new User("qwer@qwer.qwer", "firsName1", "lastName1", 34, passwordEncoder.encode("qwer"));
-        admin.addRole(roleService.add(roleAdmin));
+        UserCreateUpdateDto admin =
+                new UserCreateUpdateDto("qwer@qwer.qwer", "qwer", "qwer", 34, "qwer", Set.of(roleAdmin));
         userService.add(admin);
 
-        User user = new User("asdf@asdf.asdf", "firsName2", "lustName2", 23, passwordEncoder.encode("asdf"));
-        user.addRole(roleService.add(roleUser));
+        UserCreateUpdateDto user =
+                new UserCreateUpdateDto("asdf@asdf.asdf", "asdf", "asdf", 23, "asdf", Set.of(roleUser));
         userService.add(user);
 
-        User userAdmin = new User("zxcv@zxcv.zxcv", "firsName3", "lustName3", 45, passwordEncoder.encode("zxcv"));
-        userAdmin.addRole(roleService.add(roleUser));
-        userAdmin.addRole(roleService.add(roleAdmin));
+        UserCreateUpdateDto userAdmin =
+                new UserCreateUpdateDto("zxcv@zxcv.zxcv", "zxcv", "zxcv", 45, "zxcv", Set.of(roleAdmin, roleUser));
         userService.add(userAdmin);
     }
 }
